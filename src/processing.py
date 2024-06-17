8af37166cc47987abdff44ee5d4f45283d20112d
Nikolay_Lyalkin <serega94nn@yandex.ru>
2024-06-17 10:41:05 +0300

    add func sort_by_date() in src/processing.py

--- src/processing.py
+++ src/processing.py
@@ -4,3 +4,10 @@ def filter_by_state(info_operations: list[dict], state: str = "EXECUTED") -> lis
     result_operations = []
     [result_operations.append(info) for info in info_operations if info["state"] == state]
     return result_operations
+
+
+def sort_by_date(info_operations: list[dict], reverse: bool = True) -> list[dict]:
+    """сортировка операций по дате"""
+
+    result_sort = sorted(info_operations, key=lambda x: x["date"], reverse=reverse)
+    return result_sort
