1a36d0abd51bfa57bc5eea65ec50eacefb8a2eb9
Nikolay_Lyalkin <serega94nn@yandex.ru>
2024-07-12 16:19:40 +0300

    добавлено логирование

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,22 @@
+import logging
+
+filepath_logger = "C:\\Users\\sereg\\OneDrive\\Рабочий стол\\bank_project\\logs\\masks.log"
+logger = logging.getLogger("masks")
+file_handler = logging.FileHandler(filepath_logger, "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
+
 def get_mask_card_number(num_card: str) -> str:
     """возвращает маску карты"""
 
     num_card = str(num_card)
     if len(num_card) != 16 or not num_card.isdigit():
+        logger.info(f"Номер карты - {num_card} не соответствует требованиям")
         return "Вы ввели неверный номер карты"
+    logger.info(f"Была выполнена маскировка номера карты {num_card}")
     return f"{num_card[0:4]} {num_card[4:6]}** **** {num_card[-4:]}"
 
 
@@ -12,5 +25,10 @@ def get_mask_account(num_account: str) -> str:
 
     num_account = str(num_account)
     if len(num_account) != 20 or not num_account.isdigit():
+        logger.info(f"Номер счёта - {num_account} не соответствует требованиям")
         return "Вы ввели неверный номер счёта"
+    logger.info(f"Была выполнена маскировка номера счёта {num_account}")
     return f"**{num_account[-4:]}"
+
+
+print(get_mask_card_number("1234123412341234"))
--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,31 @@
 import json
+import logging
 from typing import Any
 
 from src.external_api import convertation
 
+filepath_logger = "C:\\Users\\sereg\\OneDrive\\Рабочий стол\\bank_project\\logs\\utils.log"
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler(filepath_logger, "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
 
 def get_data_by_operations(filename: str) -> list[dict]:
-    """Принимает путь к json файлу и возвращает словарь с данными"""
+    """Принимает путь к json файлу и возвращает список с вложенными словарями с информацией о транзакциях"""
     json_data_transaction = []
     try:
         with open(filename, "r", encoding="utf-8") as f_obg:
             json_data_transaction = json.load(f_obg)
+            logger.info("Спискок с вложенными словарями с информацией о транзакциях успешно создан")
             return json_data_transaction
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as ex:
+        logger.error(f"{filename} вызывает {ex}")
         return json_data_transaction
-    except FileNotFoundError:
+    except FileNotFoundError as ex:
+        logger.error(f"{filename} вызывает {ex}")
         return json_data_transaction
 
 
@@ -24,5 +36,7 @@ def amount_transaction(transaction: dict) -> Any:
         to_currency = "RUB"
         amount = transaction["operationAmount"]["amount"]
         convert = convertation(to_currency, from_currency, amount)
+        logger.info("Конвертация выполнена!")
         return convert
+    logger.info(f"Конвертация из валюты {transaction['operationAmount']['currency']['code']} выполнена!")
     return transaction["operationAmount"]["amount"]
